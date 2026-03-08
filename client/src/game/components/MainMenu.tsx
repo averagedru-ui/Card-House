94eb554cc844fe9a7d79e8e6040c105a36768062
averagedru-ui <averagedru@gmail.com>
2026-03-08 01:12:43 -0700

    Add version number display to MainMenu component

--- client/src/game/components/MainMenu.tsx
+++ client/src/game/components/MainMenu.tsx
@@ -142,6 +142,7 @@ export const MainMenu: React.FC<MainMenuProps> = ({ onMultiplayer, onRules, onPr
             </button>
           </div>
         </div>
+        <p className="text-gray-600 text-[10px] mt-3">v1.1.0</p>
       </div>
     </div>
   );
